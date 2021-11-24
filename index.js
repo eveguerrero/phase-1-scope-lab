e6f18aa240f0b451f31aba332ded227bd1e8427e
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-11-24 02:19:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName =  customerName.toUpperCase();
+    return customerName;
+}
+
+var bestCustomer = 'me';
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+var leastFavoriteCustomer = "Me";
+
+const changeLeastFavoriteCustomer = ()=>{
+   leastFavoriteCustomer = "Ye";
+    return leastFavoriteCustomer;
+}
+
+
+
+
